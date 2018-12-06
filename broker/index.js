92b3682bcd8a53d7bdf942f02271831c01a6b9b9
leandro.narosky <leandro.narosky@gmail.com>
2018-12-05 21:44:24 -0300

    Added healthcheck for startup

--- broker/index.js
+++ broker/index.js
@@ -8,6 +8,22 @@ const net = require('net'),
 
 setInterval(queueRegistry.processQueues.bind(queueRegistry), process.env.PROCESS_QUEUE_TIME)
 
+console.log(process.env.BROKER_HOST);
+
 server.listen(process.env.BROKER_PORT, process.env.BROKER_HOST);
 console.log("Broker up");
+
+const express = require('express');
+
+const app = express();
+
+app.set('port', process.env.HEALTH_PORT || 3000);
+
+app.get("/health", (req, res, next) => res.send({status: 'UP'}));
+
+const health_port = process.env.HEALTH_PORT || 3000;
+
+app.listen(health_port, function () {
+  console.log(`Health check running on ${health_port} !`);
+});
 
